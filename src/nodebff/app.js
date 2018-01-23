dd6c69b2fba3a6737d69bafd2da221317bffc55a
Charley <charleylla@foxmail.com>
2018-01-23 16:09:46 +0800

    文件夹拆分

--- src/nodebff/app.js
+++ src/nodebff/app.js
@@ -3,33 +3,16 @@ import router from "koa-simple-router";
 import render from "koa-swig";
 import koaStatic from "koa-static";
 import co from "co";
-import log4js from "log4js";
-import { createContainer,Lifetime } from "awilix";
-import { scopePerRequest,loadControllers } from "awilix-koa";
 
-// import InitController from "./controllers/InitController";
+import CoreLogError from "./core/CoreLogError";
+import CoreDI from "./core/CoreDI";
 import CONFIG from "./config";
-import ErrorHandler from "./middlewares/ErrorHandler"
 
-const app = new Koa();
 const PORT = CONFIG.get("PORT");
 const VIEWS_DIR = CONFIG.get("VIEWS_DIR");
 const STATIC_DIR = CONFIG.get("STATIC_DIR");
-log4js.configure({
-    appenders: { errors: { type: 'file', filename: './logs/errors.log' } },
-    categories: { default: { appenders: ['errors'], level: 'error' } }
-});
-const logger = log4js.getLogger();
-const container = createContainer();
-// 将 Service 注册到容器，缓存 Service 实例
-container.loadModules(["services/*Service.js","po/*Po.js"], {
-    formatName: "camelCase",
-    resolverOptions: {
-      lifetime: Lifetime.SCOPED
-    },
-    cwd:__dirname
-});
-// app.use(scopePerRequest(container))
+
+const app = new Koa();
 // 让 ctx 拥有渲染页面的能力
 app.context.render = co.wrap(render({
     root: VIEWS_DIR,
@@ -41,10 +24,17 @@ app.context.render = co.wrap(render({
 }));
 
 app.use(koaStatic(STATIC_DIR));
-// new InitController(app,router);
-new ErrorHandler(app,logger);
-app.use(scopePerRequest(container))
-app.use(loadControllers("controllers/*Controller.js",{cwd:__dirname}))
+
+// 启用日志和错误中间功能
+new CoreLogError(app);
+// 启用依赖注入功能
+new CoreDI(app,{
+    cwd:__dirname,
+    servicePath:"./services/*Service.js",
+    poPath:"./po/*Po.js",
+    controllerPath:"./controllers/*Controller.js"
+});
+
 app.listen(PORT,()=>{
     console.info(`Server is running at ${PORT}.`)
 });
